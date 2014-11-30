8140d5236b7f551e8719dfd94ad7f8ae4257cccd
eric <eric@netcosports.com>
2014-11-30 11:59:55 +0100

    nullpointer fixed

--- library/src/com/espian/showcaseview/targets/ViewTarget.java
+++ library/src/com/espian/showcaseview/targets/ViewTarget.java
@@ -19,9 +19,13 @@ public class ViewTarget implements Target {
     @Override
     public Point getPoint() {
         int[] location = new int[2];
-        mView.getLocationInWindow(location);
-        int x = location[0] + mView.getWidth() / 2;
-        int y = location[1] + mView.getHeight() / 2;
+        int x = 0;
+        int y = 0;
+        if (mView != null) {
+            mView.getLocationInWindow(location);
+            x = location[0] + mView.getWidth() / 2;
+            y = location[1] + mView.getHeight() / 2;
+        }
         return new Point(x, y);
     }
 }
